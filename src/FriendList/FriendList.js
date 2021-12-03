2dadf7d7e4495948d7c5dcba3043da9ea553dacf
Yurii Kuliasov <mr.awesome@i.ua>
2021-12-03 06:18:47 +0200

    app element

---
+++ src/FriendList/FriendList.js
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import FriendListItem from './FriendListItem.js';
+import s from './FriendList.module.scss';
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul className={s.FriendList}>
+      {friends.map(friend => (
+        <FriendListItem
+          avatar={friend.avatar}
+          name={friend.name}
+          isOnline={friend.isOnline}
+          key={friend.id}
+        />
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    }),
+  ).isRequired,
+};
+
+export default FriendList;
