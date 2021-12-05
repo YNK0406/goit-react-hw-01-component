c0063c67dec7efd962b6651be970d58f4d0f743a
Yurii Kuliasov <mr.awesome@i.ua>
2021-12-05 20:44:49 +0200

    +

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Section from 'components/Section/Section';
 import user from 'components/Profile/user.json';
-// import data from './components/Statistics/data.json';
+import data from './components/Statistics/data.json';
 // import transactions from'./components/TransactionHistory/transactions.json';
 // import friends from './components/FriendList/friends.json';
 import Profile from 'components/Profile/Profile';
+
 // import FriendList from './components/FriendList/FriendList';
 // import TransactionHistory from './components/TransactionHistory/TransactionHistory';
-// import Statistics from './components/Statistics/Statistics';
+import Statistics from './components/Statistics/Statistics';
 
 export default function App() {
   return (
@@ -20,16 +21,15 @@ export default function App() {
         avatar={user.avatar}
         stats={user.stats}/>
     </Section>
-    {/* <Section>
-      <Statistics title="Upload stats" stats={data} />;
-      {/* <Statistics stats={data} /> */}
-    {/* </Section>
-    <Section>
+   <Section>
+   <Statistics title="Upload stats" stats={data} />
+   </Section>
+   {/* <Section>
       <FriendList friends={friends} />
-    </Section>
-    <Section>
-      <TransactionHistory items={transactions} />;
-    </Section> */} 
+     </Section>
+     <Section>
+       <TransactionHistory items={transactions} />;
+     </Section>  */}
     </>
   )
 }
--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -11,7 +11,7 @@ const Section = ({children}) => {
 };
 
 Section.propTypes = {
-   children: PropTypes.node,
+   children:PropTypes.node,
 };
 
 export default Section;
