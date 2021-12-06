a9a1296906b111dc11f18b9c309c6a16599f8083
Yurii Kuliasov <mr.awesome@i.ua>
2021-12-06 05:44:12 +0200

    +

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -4,30 +4,30 @@ import PropTypes from "prop-types";
 import s from './Statistics.module.scss';
 
 const Statistics = ({ title, stats }) => {
-   return (
-     <div className={s.Statistics}>
-       {title && <h2 className={s.Title}>{title}</h2>}
- 
-       <ul className={s.List}>
-         {stats.map(stat => (
-           <StatisticsItem
-             key={stat.id}
-             label={stat.label}
-             percentage={stat.percentage}
-             background={stat.background}
-           />
-         ))}
-       </ul>
-     </div>
-   )
- };
- 
+    return (
+      <div className={s.Statistics}>
+        {title && <h2 className={s.Title}>{title}</h2>}
+  
+        <ul className={s.List}>
+          {stats.map(stat => (
+            <StatisticsItem
+              key={stat.id}
+              label={stat.label}
+              percentage={stat.percentage}
+              background={stat.background}
+            />
+          ))}
+        </ul>
+      </div>
+    )
+  };
+
 
 Statistics.propTypes = {
-   title: PropTypes.string,
-   stats: PropTypes.arrayOf(PropTypes.shape({
-      id: PropTypes.string.isRequired,}),
-   ),
+    title: PropTypes.string,
+    stats: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.string.isRequired,}),
+    ),
 };   
 
 export default Statistics;
